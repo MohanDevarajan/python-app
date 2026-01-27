1ec59d41fa06fe74d4f2be163eac75ec35ecdf2b
MohanDevarajan <mohandevarajan1@gmail.com>
2026-01-27 12:11:35 +0000

    feat: testing github runners

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
         'hostname': (socket.gethostname()),
-        'message': 'you are doing well'
+        'message': 'you are doing well, Mohan'
     })
 
 @app.route('/api/v1/healthz')
